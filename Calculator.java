b5d3e45187abcf1325748910574ff857041a236f
JSRosu <88613736+JSRosu@users.noreply.github.com>
2021-08-11 15:55:01 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+package week1.day2;
+ 
+
+public class Calculator {
+
+	public int add() {
+		int num1 = 12;
+		int num2 = 21;
+		int num3 = 1;
+		int addition = num1+num2+num3;
+		return addition;
+	}
+	
+	public int sub() {
+		int num1 = 54;
+		int num2 = 10;
+		int subtraction=num1-num2;
+		return subtraction;
+}
+	public double mul() {
+		double num1 = 12345563;
+		double num2=124543;
+		double multiplication=num1*num2;
+		return multiplication;
+	}
+	public float div() {
+		float num1 = 12.10f;
+		float num2=1.23f;
+		float division = num1/num2;
+		return division;
+	}
+	
+}
